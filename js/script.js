5f989c0e9bcaee06c26d410338a9bef779d5a1c0
Jakob Košir <kosir.jakob@gmail.com>
2016-01-27 15:06:16 +0100

    123 polizei

--- js/script.js
+++ js/script.js
@@ -1,61 +1,69 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(
+    75,           // Field of view
+    window.innerWidth / window.innerHeight, // Aspect ratio
+    1,            // Near clipping plane
+    10000         // Far clipping plane
+);
+// Distance camera from the center of the scene.
+camera.position.z = 4;
+
+// Rotate
+camera.rotation.x = 0.4;
+const planeMaterial = new THREE.MeshPhongMaterial({
+    specular: 0xfb8717,         // Specular color of the material (light)
+    color: 0xFF4E50,            // Geometry color in hexadecimal
+    emissive: 0xFF4E50,         // Emissive color of the material (dark)
+    shininess: 30,              // How shiny the specular highlight is
+    shading: THREE.FlatShading  // NoShading, FlatShading or SmoothShading
+});
+const planeGeometry = new THREE.PlaneGeometry(30, 60, 60, 120);
+planeGeometry.vertices.map(function (vertex) {
+    vertex.x += -.5 + Math.random() / 10;
+    vertex.y += -.5 + Math.random() / 10;
+    vertex.z = -.5 + Math.random() / 5;
+    return vertex;
+});
+
+// Update geometry.
+planeGeometry.computeFaceNormals();
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+// Create a wireframe
+const wireframeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xFF4E50,
+    wireframe: true
+});
+const wireframe = new THREE.Mesh(planeGeometry, wireframeMaterial);
+scene.add(wireframe);
+scene.add(plane);
+
+var light = new THREE.DirectionalLight(0xffffff, 0.3);
+light.position.set(1, 1, 1);
+scene.add(light);
 
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-renderer.setClearColor(0xB7B7B7);
 
-var geometry = new THREE.PlaneGeometry(5, 7);
-var material = new THREE.MeshPhongMaterial({color:0xeeeeee,side:THREE.DoubleSide});
-var planes = [];
-
-var directionalLight = new THREE.DirectionalLight( 15953704, 1.5 );
-directionalLight.position.set( 0, 1, 0 );
-scene.add( directionalLight );
-
-var directionalLight2 = new THREE.DirectionalLight( 15953704, 0.5 );
-directionalLight2.position.set( 1, 1, 1 );
-scene.add( directionalLight2 );
-
-var light = new THREE.PointLight(15953704,2.8,140);
-//light.position.set(0,0,100);
-//scene.add(light);
-
-THREE.Mesh.prototype.move = function(){
-  this.position.x += this.xSpeed;
-  this.position.y += this.ySpeed;
-  //this.rotation.y += this.yRotation;
-};
-
-function getPlane() {
-  var plane = new THREE.Mesh(geometry, material);
-  plane.xSpeed = Math.random();
-  plane.ySpeed = Math.random();
-  plane.rotation.x = Math.random()*5.5;
-  plane.rotation.y = Math.random()*5.5;
-  plane.position.set(
-    (Math.random() - 1) * 150 ,
-    (Math.random() - 1) * 90,
-    (Math.random() - 1) * 90
-  );
-  return plane
-}
-for (var i = 0; i < 155; i++) {
-  var plane = getPlane();
-  scene.add(plane);
-  planes.push(plane);
-}
-
-
-camera.position.z = 100;
 
 function render() {
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
-  for (var i = 0; i < planes.length; i++) {
-    planes[i].move();
-  }
+    requestAnimationFrame(render);
+    light.position.x = -1 + (mouseX / window.innerWidth) * 2;
+    light.position.y = 1 - (mouseY / window.innerHeight) * 2;
+    renderer.render(scene, camera);
 }
-
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize, false);
 render();
+var mouseX = 0;
+var mouseY = 0;
+function onMouseMove(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+}
+window.addEventListener('mousemove', onMouseMove, false);
